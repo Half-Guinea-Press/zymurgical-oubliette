bcbef6e83d83e0117632802d4a3528bb75ef595f
madlogmay <71596330+madlogmay@users.noreply.github.com>
2024-08-20 13:30:02 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,61 +1,121 @@
 import dash
+import dash_auth
+import dash_bootstrap_components as dbc
+import numpy as np
 import pandas as pd
 from dash import Dash, dash_table, dcc, html, Input, Output, State
+from dash_bootstrap_components._components.Container import Container
+import plotly.graph_objects as go
 import plotly.express as px
+import json
 
 app = Dash(__name__)
 server = app.server
 
-df = px.data.gapminder()
+# Get Data
 
-range_slider = dcc.RangeSlider(
-    value=[1987, 2007],
-    step=5,
-    marks={i: str(i) for i in range(1952, 2012, 5)},
-)
+@app.callback(Output("sidebars","is_open",allow_duplicate=True),
+    [Input("sidebar-toggle","n_clicks)],
+    [State("sidebars","is_open")],
+    prevent_initial_call=True
+    )
+def toggle_sidebar(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
-dtable = dash_table.DataTable(
-    columns=[{"name": i, "id": i} for i in sorted(df.columns)],
-    sort_action="native",
-    page_size=10,
-    style_table={"overflowX": "auto"},
-)
+@app.callback(Output('page-content','children'),
+    [Input('url','pathname')])
+def render_page_content(pathname):
+    if pathname == '/':
+        return [main_page]
+    elif pathname == '/pc/':
+        return [pc_page]
+    elif pathname == '/character_design/':
+        return [character_design_page]
+    return dbc.Card([
+        html.H1('404: Not found'),
+        html.Hr(),
+        html.P(f'the pathname {pathname} was not recognized ...'),
+        ])
 
-download_button = html.Button("Download Filtered CSV", style={"marginTop": 20})
-download_component = dcc.Download()
+pc_page = html.Div([])
 
-app.layout = html.Div(
+character_design_page = html.Div([])
+
+main_page = html.Div([
+    dbc.Navbar(
+        dbc.Container(
+            [
+                html.A(
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Img(src="", height="30px")),
+                            dbc.Col(dbc.NavbarBrand("Brand Text")),
+                        ],
+                        align="center",
+                        className="g-0",
+                    ),
+                    href="/",
+                    style={"textDecoration":"none"},
+                ),
+            ]
+        ),
+        color="dark",
+        dark=True,
+    ),
+    dbc.Row([
+        dbc.Col([
+            dbc.Button("<>",id="sidebar-toggle", className="open", n_clicks=0, style={"zIndex":"+1","position":"relative","top":"=10px"}),
+            dbc.Collapse(
+                [
+                    dbc.Nav(
+                        [
+                            dbc.NavLink("Home", href="/", active="exact"),
+                            dbc.NavLink("PC", href="/pc/", active="exact"),
+                            dbc.NavLink("Character Design", href="/character_design/", active="exact"),
+                        ],
+                        vertical=True,
+                        pills=True,
+                    )
+                ],
+                id="sidebars",
+                    className="open",
+                    is_open=True,
+                    dimension="width",
+                    style={
+                        "position"="fixed",
+                        "top":100,
+                        "left":0,
+                        "bottom":0,
+                        "width":"inherit",
+                        "padding":"1rem 1rem",
+                        "backgroundColor": "#f8f9fa",
+                    },
+                ),
+            ],
+            width=2,
+        ),
+        dbc.Col(
+            html.Div([
+                html.Div(id="dd-output-container", style={'paddingTop':'10px'}),
+                html.Div(id='page-content',children=[],style{'display':'block'}),
+            ]),width=9),
+        ],style={'paddingTop':'10px'}),
+    dcc.Location(id='url')])
+
+app.layout = main_page
+
+app.validation_layout = html.Div(
     [
-        html.H2("Gapminder Data Download", style={"marginBottom": 20}),
-        download_component,
-        range_slider,
-        download_button,
-        dtable,
+        main_page,
+        pc_page,
+        character_design_page
     ]
 )
 
-
-@app.callback(
-    Output(dtable, "data"),
-    Input(range_slider, "value"),
-)
-def update_table(slider_value):
-    if not slider_value:
-        return dash.no_update
-    dff = df[df.year.between(slider_value[0], slider_value[1])]
-    return dff.to_dict("records")
-
-
-@app.callback(
-    Output(download_component, "data"),
-    Input(download_button, "n_clicks"),
-    State(dtable, "derived_virtual_data"),
-    prevent_initial_call=True,
-)
-def download_data(n_clicks, data):
-    dff = pd.DataFrame(data)
-    return dcc.send_data_frame(dff.to_csv, "filtered_csv.csv")
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__=='__main__':
+    app.run_server
+    
+url = f'https://zymurgical-oubliette.onrender.com/'
+                        
