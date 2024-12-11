6ed0791c31d7a30345a60cefd67995680cd1029d
madlogmay <71596330+madlogmay@users.noreply.github.com>
2024-12-11 11:32:43 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
+#host/app.py
+
 from flask import Flask, render_template_string, g
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from dash_apps import simple_app, population
+import ssl
 import pandas as pd
+from dash_apps import simple_app, population
+app = Flask(__name__)
 
-# Initialize Flask server
-flask_app = Flask(__name__)
-
-with flask_app.app_context():
+with app.app_context():
     # Define Flask context variables to be used in apps. 
     # In this case, we define the dataframe used in the Population app (df)
     # and the Flask instance to be passed to both apps (cur_app)
@@ -14,16 +14,13 @@ with flask_app.app_context():
         "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
     )
 
-    # Integrate Flask and Dash apps using DispatcherMiddleware
-    # The app becomes a Middleware instance, which will default to the 
-    # routes in the Flask app if the URL is not found among the mounted
-    # Dash apps
-    app = DispatcherMiddleware(flask_app, {
-        "/simple_app": simple_app.init_app("/simple_app/"),
-        "/population": population.init_app("/population/")
-    })
+    g.cur_app = app
 
-@flask_app.route("/")
+    # Add Dash app to Flask context. Specify the app's url path and pass the flask server to your data
+    app = simple_app.init_app("/simple_app/")
+    app = population.init_app("/population/")
+
+@app.route("/")
 def home():
     return render_template_string(f"""
         <h1>Main Flask App</h1>
